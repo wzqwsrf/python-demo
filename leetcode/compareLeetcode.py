95eb7e1652e61f94ecf7b4997b41a3a3984a74f0
wangzhenqing <wzqwsrf@126.com>
2015-07-02 17:59:36 +0800

    compare leetcode and github

--- leetcode/compareLeetcode.py
+++ leetcode/compareLeetcode.py
@@ -1,58 +1,109 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 # author: wangzhenqing <wangzhenqing1008@163.com>
-# date: 2015-06-29 16:50:53
+# date: 2015-07-01 17:26:57
+
+import os
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 
-import re
-import requests
-# 获取网页内容
-r = requests.get('https://leetcode.com/problemset/algorithms/')
-data = r.text
-# 利用正则查找所有连接
-linkList = re.findall(r"(?<=href=\").+?(?=\")|(?<=href=\').+?(?=\')", data)
-allList1 = []
-for url in linkList:
-    if url[1:9] == 'problems':
-        urlInfo = url.split('/')
-        allList1.append(urlInfo[2])
+# 写文件
+def write_file(list3):
+    file_object = open('not_in_github.txt', 'w')
+    for name in list3:
+        file_object.write(name + '\n')
+    file_object.close()
 
 
-r = requests.get('https://github.com/wzqwsrf/Leetcode/tree/master/Java')
-data = r.text
-# 利用正则查找所有连接
-linkList = re.findall(r"(?<=href=\").+?(?=\")|(?<=href=\').+?(?=\')", data)
-# linkList = re.findall(r"('/wzqwsrf/Leetcode/blob/master/Java/')", data)
-allList2 = []
-for url in linkList:
-    prefix = '/wzqwsrf/Leetcode/blob/master/Java/'
-    if prefix in url:
-        url = url.replace(prefix, '')
-        url = url.replace('.java', '')
-        url = url.replace('_', '-')
-        url = url.lower()
-        allList2.append(url)
+# 读取txt文件
+def get_leetcode_names():
+    list1 = []
+    file_object = open('leetcode.txt')
+    try:
+        lines = file_object.readlines()
+        for line in lines:
+            line_array = line.split('||')
+            prob_name = line_array[1]
+            prob_id = line_array[0]
+            if len(prob_id) == 1:
+                prob_id = '00' + prob_id
+            elif len(prob_id) == 2:
+                prob_id = '0' + prob_id
+            full_name = prob_id + ' ' + prob_name
+            list1.append(full_name)
+    finally:
+        file_object.close()
+    return list1
 
-print 'sort-colors' not in allList1
-print 'sort-colors' not in allList2
 
-file_object = open('3.txt', 'w')
-for url in allList1:
-    print url
-    if url not in allList2:
-        file_object.write(url)
-        file_object.write('\n')
-file_object.close()
+# 读取txt文件
+def get_github_names():
+    list2 = []
+    file_object = open('github.txt')
+    try:
+        lines = file_object.readlines()
+        for line in lines:
+            list2.append(line.replace('\n', '').strip().replace('.java', ''))
+    finally:
+        file_object.close()
+    return list2
 
-# file_object = open('1.txt', 'w')
-# for url in allList1:
-#     file_object.write(url)
-#     file_object.write('\n')
-# file_object.close()
-#
-# file_object = open('2.txt', 'w')
-# for url in allList2:
-#     file_object.write(url)
-#     file_object.write('\n')
-#
-# file_object.close()
+
+# 获取没有提交的题目列表
+def get_not_github():
+    list3 = []
+    list1 = get_leetcode_names()
+    list2 = get_github_names()
+    for name in list1:
+        if name not in list2:
+            list3.append(name)
+    write_file(list3)
+
+
+# 创建文件
+def create_files(prob_dict):
+    file_object = open('not_in_github.txt')
+    for line in file_object.readlines():
+        file_write = open(line + '.java', 'w')
+        file_write.write('\n')
+        file_write.write('//' + line[3:] + '\n')
+        file_write.write('/**' + '\n')
+        file_write.write(' * @author:wangzq' + '\n')
+        file_write.write(' * @email:wangzhenqing1008@163.com' + '\n')
+        file_write.write(' * @date:2015-07-02 16:57:36' + '\n')
+        file_write.write(' * @url:' + prob_dict[line.replace('\n', '')])
+        file_write.write(' */' + '\n')
+    file_object.close()
+
+
+# 读取txt文件
+def get_filename_by_probname():
+    probname_dict = {}
+    file_object = open('leetcode.txt')
+    try:
+        lines = file_object.readlines()
+        for line in lines:
+            line_array = line.split('||')
+            prob_name = line_array[1]
+            prob_id = line_array[0]
+            if len(prob_id) == 1:
+                prob_id = '00' + prob_id
+            elif len(prob_id) == 2:
+                prob_id = '0' + prob_id
+            full_name = prob_id + ' ' + prob_name
+            # if 'Best Time to Buy and Sell Stock IV' in full_name:
+            #     print line_array
+            #     print line_array[2]
+            probname_dict[full_name] = line_array[2]
+    finally:
+        file_object.close()
+    return probname_dict
+
+
+if __name__ == '__main__':
+    # prob_dict = get_filename_by_probname()
+    get_not_github()
+    print 'over'
---
+++ leetcode/github.py
@@ -0,0 +1,57 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: wangzhenqing <wangzhenqing1008@163.com>
+# date: 2015年07月02日15:53:50
+
+import requests
+from BeautifulSoup import BeautifulSoup as soup
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+# 获取leetcode列表
+def get_github_url():
+    urls = []
+    # 获取网页内容
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/43.0.2357.130 Safari/537.36'
+    }
+    cur_url = 'https://github.com/wzqwsrf/Leetcode/tree/master/Java'
+    s = requests.session()
+    r = s.get(cur_url, headers=headers)
+    # 这里主要转义一些特殊符号，如<会有问题
+    r.encoding = 'utf-8'
+    html = soup(r.text, convertEntities=soup.HTML_ENTITIES)
+    problem_list = html.findAll('table', {"class": "files"})
+    tbody = problem_list[0].find('tbody')
+    common_url = 'https://github.com'
+    spans = tbody.findAll('span', {"class": "css-truncate css-truncate-target"})
+    k = 0
+    slen = len(spans)
+    for x in xrange(0, slen, 3):
+        span = spans[x]
+        print span
+        a = span.find('a')
+        title = a['title']
+        url = a['href']
+        urls.append(title)
+    return urls
+
+
+# 写文件
+def write_file(urls):
+    file_object = open('github.txt', 'w')
+    for url in urls:
+        file_object.write(url + '\n')
+    file_object.close()
+
+
+if __name__ == '__main__':
+    urls = get_github_url()
+    write_file(urls)
+    print 'over'
